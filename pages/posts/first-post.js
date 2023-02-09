605f828fbc344814a1d6c595c24f1e1a42576621
anuj <anujthakur315@gmail.com>
2023-02-09 22:12:53 +0530

    learn about link and pages in next.js

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+
+function FirstPost() {
+  return (
+    <>
+    
+    <h1>Firstpost</h1>
+    <h2><Link href="/">Back to home</Link></h2>
+    </>
+    
+  )
+}
+
+export default FirstPost
